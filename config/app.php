80b8bc355bdd83163b883242e6fe7a80e84bd404
Pablo Peña G <43855881+Justice2197@users.noreply.github.com>
2021-05-03 11:32:55 -0400

    Intervantion Image Installation

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -234,6 +235,7 @@ return [
         'FastExcel' => Rap2hpoutre\FastExcel\Facades\FastExcel::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Image' => \Intervention\Image\Facades\Image::class,
     ],
 
 ];
