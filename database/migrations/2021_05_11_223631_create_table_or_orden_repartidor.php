2ce2ba3bb0b388a93cb4a002e0ca57e7c5a89d88
patriciacampos <paathyfernandacg03@gmail.com>
2021-05-11 23:32:42 -0400

    Migracion y Modelo orden repartidor

---
+++ database/migrations/2021_05_11_223631_create_table_or_orden_repartidor.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableOrOrdenRepartidor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('or_orden_repartidor', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_usuario')->references('id')->on('s_usuario');
+            $table->foreignId('id_repartidor')->references('id')->on('s_usuario');
+            $table->foreignId('id_orden')->references('id')->on('or_orden');
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('or_orden_repartidor');
+    }
+}
