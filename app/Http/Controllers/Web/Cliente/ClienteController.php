d4970ceda22ddfe35ba591cced37b511da91378c
Pablo Peña G <43855881+Justice2197@users.noreply.github.com>
2021-05-21 18:13:01 -0400

    Cambios menores

--- app/Http/Controllers/Web/Cliente/ClienteController.php
+++ app/Http/Controllers/Web/Cliente/ClienteController.php
@@ -13,59 +13,66 @@ use App\Models\Sistema\Region;
 use App\Models\Sistema\Comuna;
 use App\Models\Sistema\Direccion;
 use App\Models\Sistema\Sistema;
+use App\Services\Policies\Web\Cliente\ClientePolicy;
 use Auth;
 
-class ClienteController extends Controller
+class ClienteController extends Controller 
 {
 
-    //Perfil Cliente
-    public function cliente() {
-      $cliente = current_client();
-      return view('web.cliente.home.cliente',compact('cliente'));
+  private $policy;
+
+  public function __construct() {
+    $this->policy = new ClientePolicy();
+  }
+
+  //Perfil Cliente
+  public function cliente() {
+    $cliente = current_client();
+    return view('web.cliente.home.cliente',compact('cliente'));
+  }
+
+
+  public function direcciones() {
+    $comunas = Comuna::orderBy('nombre')->get();
+    $regions = Region::get();
+    $cliente = current_client();
+    return view('web.cliente.home.direcciones',compact('cliente','comunas','regions'));
+  }
+
+
+  public function historial() {
+    // $comunas = Comuna::orderBy('nombre')->get();
+    // $regions = Region::get();
+    $cliente = current_client();
+    return view('web.cliente.home.historial',compact('cliente'));
+  }
+
+
+  //Actualizar Datos
+  public function profileUpdate(Request $request) {
+    try {
+      $cliente = Cliente::find(current_client()->id);
+      $cliente->nombre = $request->input('nombre');
+      $cliente->apellido = $request->input('apellido');
+      $cliente->correo = $request->input('correo');
+      $cliente->telefono = $request->input('telefono');
+      $cliente->birthdate = date_format(date_create($request->input('birthdate')),'Y-m-d');
+      $cliente->sexo = $request->input('sexo');
+
+      // if(!empty($request->file('image'))) {
+      //   $filename = time();
+      //   $folder = 'public/photo_clientes';
+      //   $cliente->imagen = ImportImage::save($request, 'image', $filename, $folder);
+      // }
+
+      $cliente->update();
+
+      return back()->with('success','Se ha actualizado');
+    } catch (\Throwable $th) {
+      // return $th;
+      return back()->with('info','Error intente nuevamente');
     }
-
-
-    public function direcciones() {
-      $comunas = Comuna::orderBy('nombre')->get();
-      $regions = Region::get();
-      $cliente = current_client();
-      return view('web.cliente.home.direcciones',compact('cliente','comunas','regions'));
-    }
-
-
-    public function historial() {
-      // $comunas = Comuna::orderBy('nombre')->get();
-      // $regions = Region::get();
-      $cliente = current_client();
-      return view('web.cliente.home.historial',compact('cliente'));
-    }
-
-
-    //Actualizar Datos
-    public function profileUpdate(Request $request) {
-      try {
-        $cliente = Cliente::find(current_client()->id);
-        $cliente->nombre = $request->input('nombre');
-        $cliente->apellido = $request->input('apellido');
-        $cliente->correo = $request->input('correo');
-        $cliente->telefono = $request->input('telefono');
-        $cliente->birthdate = date_format(date_create($request->input('birthdate')),'Y-m-d');
-        $cliente->sexo = $request->input('sexo');
-
-        // if(!empty($request->file('image'))) {
-        //   $filename = time();
-        //   $folder = 'public/photo_clientes';
-        //   $cliente->imagen = ImportImage::save($request, 'image', $filename, $folder);
-        // }
-
-        $cliente->update();
-
-        return back()->with('success','Se ha actualizado');
-      } catch (\Throwable $th) {
-        // return $th;
-        return back()->with('info','Error intente nuevamente');
-      }
-    }
+  }
 
 
     //Actualizar contraseña
@@ -173,9 +180,10 @@ class ClienteController extends Controller
     //Index
     public function direccionesIndex($id){
       $cliente = current_client();
+      $d = Direccion::where('id_cliente',$cliente->id)->findOrFail($id);
+      $this->policy->direccionesIndex($d,$cliente);
       $comunas = Comuna::orderBy('nombre')->get();
       $regions = Region::get();
-      $d = Direccion::where('id_cliente',$cliente->id)->findOrFail($id);
       return view('web.cliente.home.direccionesEdit',compact('cliente','comunas','regions','d'));
     }
 
