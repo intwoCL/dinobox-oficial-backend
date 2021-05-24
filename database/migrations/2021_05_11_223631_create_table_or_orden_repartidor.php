708110386b9f4a0048d0d09e0b98776902b3f190
Pablo Peña G <43855881+Justice2197@users.noreply.github.com>
2021-05-24 19:51:11 -0400

    Cambio menor

--- database/migrations/2021_05_11_223631_create_table_or_orden_repartidor.php
+++ database/migrations/2021_05_11_223631_create_table_or_orden_repartidor.php
@@ -18,8 +18,6 @@ class CreateTableOrOrdenRepartidor extends Migration
             // $table->foreignId('id_usuario')->references('id')->on('s_usuario');
             $table->foreignId('id_repartidor')->references('id')->on('s_usuario');
             $table->foreignId('id_orden')->references('id')->on('or_orden');
-
-//             $table->integer('valor_estimado')->default(0);
   
             $table->integer('posicion_retiro')->nullable();
             $table->integer('posicion_despacho')->nullable();
